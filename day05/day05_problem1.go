cdf30755158390dc5590e7a4826216fec085503b
Joachim Strömbergson <joachim@assured.se>
2021-12-09 15:25:12 +0100

    Extract line coords into list of structs

--- day05/day05_problem1.go
+++ day05/day05_problem1.go
@@ -10,8 +10,15 @@ import (
 	"strings"
 	"bufio"
 	// "reflect"
-	// "strconv"
+	"strconv"
 )
+
+type line struct {
+	x1 int
+	y1 int
+	x2 int
+	y2 int
+}
 
 
 func get_input() []string {
@@ -26,12 +33,30 @@ func get_input() []string {
 }
 
 
+func get_lines(input []string) []line {
+	var my_lines []line
+	var l line
+
+	//	fmt.Println("type of input:", reflect.TypeOf(input))
+	//	fmt.Println("len(input):", len(input))
+
+	for _, line := range(input) {
+		l.x1, _ = strconv.Atoi(string(line[0]))
+		l.y1, _ = strconv.Atoi(string(line[2]))
+		l.x2, _ = strconv.Atoi(string(line[7]))
+		l.y2, _ = strconv.Atoi(string(line[9]))
+		my_lines = append(my_lines, l)
+	}
+	return my_lines
+}
+
 func main() {
 	my_input := get_input()
+	my_lines := get_lines(my_input)
 
 	fmt.Println("Solution to AoC 2021, day 05, problem 1")
 	fmt.Println("---------------------------------------")
-	fmt.Println(my_input)
+	fmt.Println("my_lines:", my_lines)
 	fmt.Println("")
 }
 
